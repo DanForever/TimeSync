4ed1b41c05870d1c39bd75237d16b891ebfa1d3f
DanForever <the.mephit@googlemail.com>
2015-10-21 22:42:12 +0200

    Updated files with the Proper Apache 2.0 license header

--- GAE/main.py
+++ GAE/main.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
+# Copyright 2015 Daniel Neve
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+
 import webapp2
 import facebook
 import facebook.admin
